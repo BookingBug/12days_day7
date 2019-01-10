f5284e15324df16bb8ade50d0b347197b5f635e1
Ed Clements <ed@bookingbug.com>
2019-01-10 10:16:50 +0000

    Show swans according to business question

--- swan/swan.component.js
+++ swan/swan.component.js
@@ -12,7 +12,8 @@ window.bbRegions = {
                 return Promise.resolve('');
             }
 
-            let template = `<img src="https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/375ee2b4-c274-e411-95bd-0050568e420d/630x355.jpg">`;
+            const swans = data.company.extra.how_many_swans;
+            let template = `<img width="270px" src="https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/375ee2b4-c274-e411-95bd-0050568e420d/630x355.jpg">`.repeat(swans);
 
             return Promise.resolve(template);
         }
