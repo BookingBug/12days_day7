b31a075089163f235fba4a310ead59c3ea9e30b4
Piotr Siatkowski <psiatkowski@bookingbug.com>
2019-03-11 14:27:02 +0000

    Update swan.component.js
    Adjusted example to what's working with current development version.

--- swan/swan.component.js
+++ swan/swan.component.js
@@ -1,21 +1,8 @@
-window.bbRegions = {
-    journey: [{
-        position: {
-            target: 'content',
-            order: 'before'
-        },
-        render({
-            data,
-            previousData
-        }) {
-            if (data.page === '') {
-                return Promise.resolve('');
+window.Configurator.registerRegion(
+    'journey-content-top', {
+        render({ data }) {
+            if (data.company && data.company.extra) {
+                return `<img width="270px" src="https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/375ee2b4-c274-e411-95bd-0050568e420d/630x355.jpg">`.repeat( data.company.extra.how_many_swans );
             }
-
-            const swans = data.company.extra.how_many_swans;
-            let template = `<img width="270px" src="https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/375ee2b4-c274-e411-95bd-0050568e420d/630x355.jpg">`.repeat(swans);
-
-            return Promise.resolve(template);
         }
-    }]
-}
+    });
